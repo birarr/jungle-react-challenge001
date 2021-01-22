e57fcea5032ac74f797da8d4adc1365420e355cc
Ubiratan da Rosa Rodrigues <ubiratan@Ubiratans-MacBook-Pro.local>
2021-01-22 15:27:46 -0300

    fix: fixing Lucas's review tips

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,38 +9,38 @@ import twitter from '../../assets/twitter.png';
 
 function Footer() {
     return(
-        <footer>
-            <div id='footer-component'>
-                <h1 id='become-nanny-h1'>Become a nanny share host today!</h1>
-                <h1 id='become-nanny-h1-mobile'>Become a nanny share host</h1>
-                <p id='footer-title'>Takes less than 5 minutes to get started</p>
-                <button id='footer-button'><div id='button-itens'><img src={calendarIcon} alt=""/><p id="create-nanny">
-                Create Your Nanny Share</p> <br/> <p id="take-less">
-                    Takes less than 5 minutes</p></div></button>
-                    <a href="">Or browse local nanny-shares</a>
+        <footer className="main-footer">
+            <div className='footer-component'>
+                <h1 className='become-nanny-h1'>Become a nanny share host today!</h1>
+                <h1 className='become-nanny-h1-mobile'>Become a nanny share host</h1>
+                <p className='footer-title'>Takes less than 5 minutes to get started</p>
+                <button className='footer-button'><span className='button-itens'><img src={calendarIcon} alt=""/><p className="create-nanny">
+                Create Your Nanny Share</p> <br/> <p className="take-less">
+                    Takes less than 5 minutes</p></span></button>
+                    <a className="nanny-shares-link" href="">Or browse local nanny-shares</a>
             </div>
-            <div id='footer-last-div'>
-                <div id='footer-nav-itens'>
-                    <img id='hapu-logo-gray' src={hapuLogoGray} alt="hapu logo gray"/>
-                    <nav id='footer-nav'>
-                        <li id='create-your-nanny'>Create Your Nanny Share</li>
-                        <li id='create-your-nanny-mobile'>Share your Nanny</li>
-                        <li id='browse-nanny'>Browse Nanny Shares</li>
-                        <li id='browse-nanny-mobile'>Browse Shares</li>
-                        <li id='become-nanny'>Become a Nanny Share Host</li>
-                        <li id='terms'>{"Terms & Privacy"}</li>
+            <div className='footer-last-div'>
+                <div className='footer-nav-itens'>
+                    <img className='hapu-logo-gray' src={hapuLogoGray} alt="hapu logo gray"/>
+                    <nav className='footer-nav'>
+                        <li className='create-your-nanny'>Create Your Nanny Share</li>
+                        <li className='create-your-nanny-mobile'>Share your Nanny</li>
+                        <li className='browse-nanny'>Browse Nanny Shares</li>
+                        <li className='browse-nanny-mobile'>Browse Shares</li>
+                        <li className='become-nanny'>Become a Nanny Share Host</li>
+                        <li className='terms'>{"Terms & Privacy"}</li>
                     </nav>
-                    <ul id='social-media'>
-                        <img src={facebook} alt="facebook logo"/>
-                        <img src={twitter} alt="twitter logo"/>
-                        <img src={instagram} alt="instagram logo"/>
+                    <ul className='social-media'>
+                        <a href="https://www.facebook.com"><img src={facebook} alt="facebook logo"></img></a>
+                        <a href="https://www.twiter.com"><img src={twitter} alt="twitter logo"></img></a>
+                        <a href="https://www.instagram.com"><img src={instagram} alt="instagram logo"></img></a>
                     </ul>
                 </div>
-                <div id='space'>
-                <div id='space-bar'></div>
+                <div className='space'>
+                <div className='space-bar'></div>
                 </div>
             </div>
-            <div id='footer-copyright'>
+            <div className='footer-copyright'>
                 <p>Copyright © 2017 Hapu PTY Limited All rights reserved</p>
             </div>
         </footer>
